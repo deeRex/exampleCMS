a43e36a363ec6565c8d7a5aee333daeb990db48e
deeRex <deeStirling04@gmail.com>
2015-11-30 19:20:18 -0500

    Wrote jquery search filter, added to script, called by all filtered pages

---
+++ js/script.js
@@ -0,0 +1,17 @@
+ $(function(){
+      $('#table-sort').tablesorter({
+        sortList:[[0,0],[1,0]]
+      });
+    });
+
+ //invokes table sort
+ 
+ var $rows = $('table tr');
+ $('.search-form').keyup(function(){
+  var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+  
+  $rows.show().filter(function(){
+    var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+    return !~text.indexOf(val);
+  }).hide();
+ });
